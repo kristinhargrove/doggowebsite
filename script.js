6e5da141954e6a42ac84ab705afb9f1fbc7c7d4d
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2020-10-06 18:29:56 -0600

    adds fetch request to random image endpoint

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ let button = document.getElementById('generate');
 
 button.addEventListener('click', function() {
     button.innerHTML = "Generating Doggo...";
+    fetch('https://dog.ceo/api/breeds/image/random')
 })
