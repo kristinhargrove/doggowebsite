9ad9d64ea7738dcc3bc79b9cf3fdf6a85e549d01
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2020-10-09 11:47:40 -0600

    added dropdown to pick specific dog images

--- script.js
+++ script.js
@@ -1,16 +1,64 @@
+//waits until the page is loaded because of the defer in the html
+
+//this fetchs the breeds list frm the API 
 fetch('https://dog.ceo/api/breeds/list')
     .then(function(response) {
-        return response.json();
+        return response.json(); //parses the Json
     })
-    .then(function(data) {
-        console.log(data);
+    .then(function(data) { //callback setup with the data from the API call
+        let breeds = data.message; //data that we need (list of breeds)
+
+        let breedList = document.getElementById('breed-list'); // grabs the selet element from the HTML so it could be targetted & assigns it to variable 
+
+        breeds.forEach(function(breed) { //callback function -
+            let breedElement = document.createElement('option'); // ctag for each breed
+            breedElement.setAttribute('value', breed); //set the option value the breed name
+            breedElement.innerHTML = breed; //set inner html to the breed name
+            breedList.appendChild(breedElement); // append to the breed list, which is the select box
+        });
+        
+
+        breedList.addEventListener('change', function(event) {
+            var breedName = event.target.value;
+            fetch(`https://dog.ceo/api/breed/${breedName}/images/random`)
+                .then(function(response) {
+                    return response.json();
+                })
+                .then (function(data) {
+                    let dogImage = document.createElement('img');
+                    dogImage.setAttribute('src', data.message);
+                    document.body.appendChild(dogImage);
+                })
+        })
+/*
+        let button = document.getElementById('generate');
+
+        button.addEventListener('click', function() {
+            button.innerHTML = "Generating Doggo...";
+            fetch(`https://dog.ceo/api/breed/${breedName}/images/random`)
+                .then(function(response) {
+                    return response.json();
+                })
+                .then (function(data) {
+                    let dogImage = document.createElement('img');
+                    dogImage.setAttriubute('src', data.message);
+                    document.body.appendChild(dogImage);
+                    button.innerHTML = "Generate Doggo...";
+                })
+        })
+
+        */
+
     })
 
-let button = document.getElementById('generate');
+//now we have a drop down on the page of the different breed list
 
-button.addEventListener('click', function() {
+//below code creates the doggo button
+/*
+
+button.addEventListener('', function() {
     button.innerHTML = "Generating Doggo...";
-    fetch('https://dog.ceo/api/breeds/image/random')
+    fetch('https://dog.ceo/api/${breedName}/image/random`')
         .then(function(response) {
             return response.json();
         })
@@ -22,3 +70,4 @@ button.addEventListener('click', function() {
         })
 })
 
+*/
